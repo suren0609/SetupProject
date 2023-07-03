dc4b5691989230593f15976a161d8f7a5711cc84
suren0609 <suro.balayan@gmail.com>
2023-07-03 18:50:00 +0400

    first-commit

---
+++ src/store/actions.ts
@@ -0,0 +1,8 @@
+
+import { createAction } from '@reduxjs/toolkit'
+
+import { GetPostsApiPayload } from './types'
+
+export const getPostsApi = createAction<GetPostsApiPayload>('getPostsApi')
+
+export const clearStore = createAction('clearStore')
