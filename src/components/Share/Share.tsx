a0f46ee9e7409d13e3cebef44be87b343faa1119
suren0609 <suro.balayan@gmail.com>
2023-08-03 18:33:48 +0400

    fixed bugs

--- src/components/Share/Share.tsx
+++ src/components/Share/Share.tsx
@@ -1,7 +1,8 @@
-import React, { FC, useState, FocusEvent } from "react";
+import React, { FC, useState, FocusEvent, useRef, useEffect } from "react";
 
 import styles from "./Share.module.scss";
 import { DeleteCard } from "components/DeleteCard";
+import { inputValueForShare } from "utils/inputValueForShare";
 
 interface IProp {
   closeShare: (e: any) => void;
@@ -9,6 +10,15 @@ interface IProp {
 
 const Share: FC<IProp> = ({ closeShare }) => {
   const [isDeleteActive, setDeleteActive] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    console.dir(inputRef.current);
+    inputRef.current?.select();
+  }, []);
+
+  console.log(inputRef);
 
   const handleDeleteActive = () => {
     setDeleteActive((prev) => !prev);
@@ -42,10 +52,11 @@ const Share: FC<IProp> = ({ closeShare }) => {
         <hr />
         <h6>Link to this card</h6>
         <input
+          ref={inputRef}
           data-name="inputOrButton"
           value={window.location.href}
           type="text"
-          autoFocus
+          readOnly={true}
           onFocus={handleFocus}
         />
         <button className={styles.btn} data-name="inputOrButton">
@@ -53,13 +64,15 @@ const Share: FC<IProp> = ({ closeShare }) => {
         </button>
         <h6>Embed this card</h6>
         <input
+          readOnly={true}
           data-name="inputOrButton"
-          value={`<blockquote class="trello-card"><a href="https:&#x2F;&#x2F;trello.com&#x2F;c&#x2F;N5722tOP">tgcjgtjhjhm</a></blockquote><script src="https://p.trellocdn.com/embed.min.js"></script>`}
+          value={inputValueForShare}
           type="text"
           onFocus={handleFocus}
         />
         <h6>Email for this card</h6>
         <input
+          readOnly={true}
           data-name="inputOrButton"
           value={`surenbalayan1+30p91d6rb3jrjaf2raq+30vl4q3exb2vvv8hkuf+12a7f8n83f@boards.trello.com`}
           type="text"
@@ -71,16 +84,18 @@ const Share: FC<IProp> = ({ closeShare }) => {
         </p>
         <hr />
         <p>Card #1</p>
-        <p onBlur={closeDeleteCard} tabIndex={0}>
+        <p>
           Added yesterday at 10:56 AM .{" "}
           <button
             className={styles.btn}
             onClick={handleDeleteActive}
+            onBlur={closeDeleteCard}
+            tabIndex={0}
             data-name="inputOrButton"
           >
             Delete
+            {isDeleteActive && <DeleteCard closeDeleteCard={closeDeleteCard} />}
           </button>
-          {isDeleteActive && <DeleteCard closeDeleteCard={closeDeleteCard} />}
         </p>
       </div>
     </div>
